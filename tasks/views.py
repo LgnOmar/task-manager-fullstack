f3a9742de068ac9b26044c1c505f66cecf324e59
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-10-01 21:12:54 +0100

    feat: Add TaskViewSet to handle API logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# Import the necessary modules
+# we need 'viewsets' to build our logic hub.
+# we also need 'IsAuthenticated' to enforce our security rules
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+
+# This is our ViewSet - the main logic controller for Tasks.
+# It inherites from ModelViewSet, which provides all the default CRUD actions
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    An API endpoint that allows users to view or edit their tasks.
+    """
+
+    #Connect the translator: This viewset will use the TaskSerializer to convert data ro and from JSON.
+    serializer_class - TaskSerializer
+
+    #Set the security rule: Only authenticated [logged-in] users can access this endpoint. no exceptions.
+    permission_classes = [IsAuthenticated]
+
+    # This function defines EXACTLY which tasks should be visible
+    def get_queryset(self):
+        """
+        This view should return a list of all the tasks for the currently authenticated user.
+        """
+
+        # self.request.user gives us the user object for the person making the request.
+        # we filter the Task objects to only include the ones owned by this user.
+
+        return Task.objects.filter(user=self.request.user)
+
+        # this function is called right before a new task is saved.
+        def perform_create(self, serializer):
+            """
+            when creating a new task, automatically assign it to the currently authenticated user.
+            """
+
+            # The serializer is about to save the data. we inject the user object into the data before it's saved. this prevents a user from creating a task for someone else.
+
+            serializer.save(user=self.request.user)
